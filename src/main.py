d6eb51c14ad116071f0a504d0e5fc5940ccb8832
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-06-13 15:27:43 +0000

    fix: main.pyのリントエラーを修正

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
-def main():
-    print("Hello from 3d-matching!")
+from utils.setup_logging import setup_logging
+
+
+def main() -> None:
+    logger = setup_logging(__name__)
+    logger.info("Hello from 3d-matching!")
 
 
 if __name__ == "__main__":
