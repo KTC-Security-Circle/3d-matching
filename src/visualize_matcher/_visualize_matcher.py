aba052f079d1127defadd7060db14cd0510468fd
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-11-23 02:04:32 +0000

    add: icpステップの追加

--- src/visualize_matcher/_visualize_matcher.py
+++ src/visualize_matcher/_visualize_matcher.py
@@ -6,6 +6,7 @@ import open3d as o3d
 import open3d.visualization.gui as o3dv_gui  # pyright: ignore[reportMissingImports]
 import open3d.visualization.rendering as o3dv_rendering  # pyright: ignore[reportMissingImports]
 
+from matcher.icp import refine_registration
 from matcher.ransac import global_registration
 from utils.setup_logging import setup_logging
 
@@ -45,6 +46,8 @@ class VisualizeMatcher:
         self.max_iter = 0
         self.voxel_size = 0
         self.is_logging = False
+        self._result = None
+        self._is_executed_icp = False
 
     def _setup_app(self) -> None:
         self.window = self.app.create_window(self.window_name, 800, 600)
@@ -56,7 +59,7 @@ class VisualizeMatcher:
         self.scene.add_geometry(TARGET_NAME, self.target.pcd, self.material)
 
         layout = o3dv_gui.Vert(0, o3dv_gui.Margins(10, 10, 10, 10))
-        layout.background_color = o3dv_gui.Color(1, 1, 1, 1)
+        layout.background_color = o3dv_gui.Color(0, 0, 0, 1)
 
         self.label = o3dv_gui.Label("RANSAC Fitness: progressing...")
 
@@ -81,20 +84,29 @@ class VisualizeMatcher:
 
     def _update_step(self) -> bool:
         """Called every frame by GUI."""
-        if self.iter_num >= self.max_iter:
-            return False  # stop ticking
+        if self.iter_num >= self.max_iter and self._is_executed_icp:
+            return False
 
-        result = global_registration(
-            self.source,
-            self.target,
-            self.voxel_size,
-            iteration=1,
-        )
-        self.label.text = "RANSAC Fitness: progressing..."
+        if self.iter_num < self.max_iter:
+            self._result = global_registration(
+                self.source,
+                self.target,
+                self.voxel_size,
+                iteration=1,
+            )
+        elif self._result is not None:
+            self._result = refine_registration(
+                self.source,
+                self.target,
+                self._result.transformation,
+                self.voxel_size,
+            )
+            self._is_executed_icp = True
 
         def _task() -> None:
-            self.label.text = f"Fitness: {result.fitness:.4f} | Inlier RMSE: {result.inlier_rmse:.4f}"
-            self.scene.add_geometry(SOURCE_NAME, self.source.pcd, self.material)
+            if self._result is not None:
+                self.label.text = f"Fitness: {self._result.fitness:.4f}"
+                self.scene.add_geometry(SOURCE_NAME, self.source.pcd, self.material)
 
         # Update source geometry (your Ply.pcd is updated inside global_registration)
         self.app.post_to_main_thread(self.window, _task)
